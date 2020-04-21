0ee4e457e471ea09b4eeda184e48f52469c43551
Abishek9 <61542313+Abishek9@users.noreply.github.com>
2020-04-21 20:29:32 +0530

    Add files via upload

---
+++ js-demo/home.js
@@ -0,0 +1,60 @@
+// show the title
+
+// let name="Preetha"
+// let price = `Hello ${name}` ;
+//  price = price.toLowerCase();
+
+// let price={
+//     firstName:'Abishek',
+//     lastName:'Preetha'
+// };
+let price = 100;
+// if(+(1.1+1.1).toFixed(2) == 2.2){
+//     showMessage('Discount');
+// }else{
+//     showMessage('No Discount');
+// }
+// let message = (price > 10)? 'Yes' : 'No';
+// showMessage(message);
+// for(let i=0;i<3;i++)
+// {
+//     console.log(i);
+// }
+ 
+/*
+    Detail complex logic...
+    Some algorithm...
+
+*/
+// console.log(price);
+
+
+// let fun = function logMessage(){
+//     console.log("Here is my First Function");
+// }
+// function logMessage(Message, anotherMessage){
+//     console.log("Here is my First1 Functions");
+// }
+
+// function mathMessage(value){
+//     let fun1=(value*2);
+    
+// }
+// let key=42;
+// function msg(value){
+    
+//     let msg1 = function(){
+//         let key = 14;
+//         console.log(key);
+//         return key;
+//     }
+
+//     let msg2 = value* msg1();
+//     console.log(key);
+//     return msg2;
+
+// }
+// let ouput=msg(2);
+// showMessage(output);
+
+changePercentageoff(40);
